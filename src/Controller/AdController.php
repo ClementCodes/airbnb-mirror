c218e8c56a00dfd0a355c33ca420ee3b7805ea0d
Clément <79195642+ClementCodes@users.noreply.github.com>
2022-11-29 17:39:22 +0100

    partie  des formulaires

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -23,7 +30,53 @@ class AdController extends AbstractController
             'ads' => $ads
         ]);
     }
+    /**
+     * 
+     * Permet de créer une seul annonce
+     * @Route("/ads/new", name="ads_create")
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $manager)
+    {
+        $ad = new Ad();
 
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        //handle request gere les donne de la requete dans le formualire recupéré
+        $form->handleRequest($request);
+
+
+        $this->addFlash(
+            "succes",
+            " l'annonce <strong> {{$ad->getTitle()}}</strong>a bien été enregistrée  !"
+        );
+
+
+        // $this->addFlash(
+        //     "succes",
+        //     " deuixeme flash"
+        // );
+
+        // $this->addFlash(
+        //     "danger",
+        //     "Message d'erreur"
+        // );
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            return   $this->redirectToRoute("ads_show", [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/new.html.twig', [
+
+            'form' => $form->createView()
+        ]);
+    }
 
 
     /**
@@ -31,11 +84,13 @@ class AdController extends AbstractController
      * Permet d'afficher une seule annonce
      * @Route("/ads/{slug}", name="ads_show")
      */
-    public function show($slug, AdRepository $repo)
+    public function show($slug, Ad $ad)
     {
 
-        $ad = $repo->findOneBySlug($slug);
+        // $ad = $repo->findOneBySlug($slug);
         //je recupere l'annonce qui correspond au slug
+
+        // on a convertit le pareamtre slug en une annonce
         return $this->render('ad/show.html.twig', [
             'ad' => $ad
         ]);
---
+++ src/Form/AdType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdType extends AbstractType
+{
+
+
+    /**
+     * Permet d'avoir la configuration de base d'un champ de texte 
+     
+     * @param string $label
+     * @param string $placeholder
+     * @return  array
+     */
+    public function getConfiguration($label, $placeholder)
+    {
+        return [
+            "label" => $label,
+            "attr" => [
+                'placeholder' =>  $placeholder
+            ]
+        ];
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                "label" => 'Titre',
+                "attr" => [
+                    'placeholder' => "tapez un sur titre pour votre annonce  ! "
+                ],
+            ])
+            ->add(
+                'slug',
+                TextType::class,
+                $this->getConfiguration("Adresse web", "Tapez l'adresse web (automatique)")
+            )
+            ->add('price', MoneyType::class, $this->getConfiguration("price", "Indiquez votre prix !"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Donnez une description globale de  l'annonce"))
+            ->add('content', TextareaType::class, $this->getConfiguration("Descritpion", "Donneaz une descritpion qui donne vraimeent envide de venir chez vous "))
+            ->add('coverImage', UrlType::class, $this->getConfiguration("url de l'image principale", "donnez l'adresse de l'image"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres", "Nombres de chambres disponibles"))
+
+
+            // Comentaire->1.1 new.html.twig
+            //  {% block _ad_images_widget %} {% endblock %}
+
+
+            ->add(
+                'images',
+                CollectionType::class,
+                [
+
+                    // la notion dentry (d'entrée ) represente un element de la collection (ici un sous formulaire)
+                    'entry_type' => ImageType::class,
+                    'allow_add' => true
+
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}
